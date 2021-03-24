a43a783241fca4f3a0dfa6dce60c410201a4ea31
nguyenquyem99aipower <nguyenquyem99.aipower@gmail.com>
2021-03-24 16:57:48 +0700

    Edit max length

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User',
     on_delete=models.CASCADE,
     )
-    body = models.TextField(max_length=100)
+    body = models.TextField()
 
     def __str__(self):
         return self.title
