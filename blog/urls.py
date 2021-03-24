abe346a57e7dd1d606b15286c865d13963050f0e
nguyenquyem99aipower <nguyenquyem99.aipower@gmail.com>
2021-03-24 16:42:01 +0700

    update add, edit and delete form

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import BlogListView, BlogDetailView
+from django.urls import include, path
+from rest_framework import routers
+from .views import BlogListView, BlogDetailView, BlogCreateView
+from .views import BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), 
+    name='post_edit'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('', BlogListView.as_view(), name='home')
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(),
+    name='post_delete'),
+    
 ]
+
